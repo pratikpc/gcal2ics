24b8dd52baee29aa8b42edeabb459d20050b20f0
Pratik Chowdhury <pratikc@live.co.uk>
2021-01-27 22:04:48 +0530

    Add null and string check

--- lib/index.ts
+++ lib/index.ts
@@ -2,19 +2,22 @@
 
 // Return the first param associated with the URL
 export function ParamFromURL(
-    url: string | URL,
+    url: string | URL | null,
     param: string
 ): string | null {
-    if (url instanceof String || typeof url === 'string')
-        return ParamFromURL(new URL(url as string), param);
+    if (url == null) return null;
+    if (typeof url === 'string')
+        return ParamFromURL(new URL(url), param);
     return url.searchParams.get(param);
 }
 
-export default function AsGoogleICSLink(
-    url: string | URL
+export function AsGoogleICSLink(
+    url: string | URL | null
 ): string {
     const src = ParamFromURL(url, 'src');
     return src == null
-        ? url.toString()
+        ? (url || '').toString()
         : `https://calendar.google.com/calendar/ical/${src}/public/basic.ics`;
 }
+
+export default AsGoogleICSLink;
